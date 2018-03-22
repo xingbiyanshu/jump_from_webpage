bb4989e4ab3d03ab337ed2b822771ae3175db71d
xingbiyanshu <xingbiyanshu@gmail.com>
2018-03-22 13:10:27 +0800

    get data from uri

--- app/src/main/java/com/kedacom/jump_from_webpage/MainActivity.java
+++ app/src/main/java/com/kedacom/jump_from_webpage/MainActivity.java
@@ -1,6 +1,8 @@
 package com.kedacom.jump_from_webpage;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -9,5 +11,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 接收来自网页的数据　
+        Intent intent = getIntent();
+//        intent.getScheme();
+//        intent.getDataString();
+        Uri uri = intent.getData();
+        if (null != uri){
+            System.out.println("article="+uri.getQueryParameter("article"));
+        }
     }
 }
